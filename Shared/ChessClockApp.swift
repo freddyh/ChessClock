538781769b007152018938a30bb0f81dee87f48d
freddyh <307890+freddyh@users.noreply.github.com>
2022-06-11 21:12:31 -0700

    Initial Commit

---
+++ Shared/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  Shared
+//
+//  Created by Freddy Hernandez Jr on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Freddy Hernandez Jr on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
