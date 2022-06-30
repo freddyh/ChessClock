d2975c1d6a54a2382aa0bc5156aedb396f3fe8cf
freddyh <307890+freddyh@users.noreply.github.com>
2022-06-29 21:28:41 -0700

    added StateObject

--- Shared/ChessClockApp.swift
+++ Shared/ChessClockApp.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 @main
 struct ChessClockApp: App {
+    @StateObject var appViewModel: AppViewModel = .init(
+        playerOne: Player(id: 1, initialTime: 10),
+        playerTwo: Player(id: 2, initialTime: 20)
+    )
+
     var body: some Scene {
         WindowGroup {
             ContentView(
-                playerOne: Player(id: 1, initialTime: 10),
-                playerTwo: Player(id: 2, initialTime: 20)
+                playerOne: appViewModel.playerOne,
+                playerTwo: appViewModel.playerTwo
             )
         }
+    }
+}
+
+class AppViewModel: ObservableObject {
+    @Published var playerOne: Player
+    @Published var playerTwo: Player
+
+    init(playerOne: Player, playerTwo: Player) {
+        self.playerOne = playerOne
+        self.playerTwo = playerTwo
     }
 }
